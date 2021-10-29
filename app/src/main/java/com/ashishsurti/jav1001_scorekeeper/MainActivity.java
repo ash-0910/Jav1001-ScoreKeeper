fa1d381eff67321bffd0fdee27f58a5ca6f27bcb
ashish <ashishsurti0910@gmail.com>
2021-10-29 19:39:49 -0400

    added functionality to the code in java

--- app/src/main/java/com/ashishsurti/jav1001_scorekeeper/MainActivity.java
+++ app/src/main/java/com/ashishsurti/jav1001_scorekeeper/MainActivity.java
@@ -3,12 +3,109 @@ package com.ashishsurti.jav1001_scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private int numPoints = 0 ;
+    private int totalpointsA = 0;
+    private int totalpointsB = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton teamAplus = findViewById(R.id.fbplusteama);
+        FloatingActionButton teamBplus = findViewById(R.id.fbplusteamb);
+        FloatingActionButton teamAminus = findViewById(R.id.fbminusteama);
+        FloatingActionButton teamBminus = findViewById(R.id.fbminusteamb);
+
+        teamAplus.setOnClickListener(this);
+        teamBplus.setOnClickListener(this);
+        teamAminus.setOnClickListener(this);
+        teamBminus.setOnClickListener(this);
+
+        teamAminus.setEnabled(false);
+        teamBminus.setEnabled(false);
+
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+
+        TextView teamAscore = findViewById(R.id.teamascore);
+        TextView teamBscore = findViewById(R.id.teambscore);
+        FloatingActionButton teamAminus = findViewById(R.id.fbminusteama);
+        FloatingActionButton teamBminus = findViewById(R.id.fbminusteamb);
+
+        switch (v.getId()){
+            case R.id.fbplusteama:
+                rbCheck();
+                totalpointsA += numPoints;
+                if(totalpointsA > 0){
+                    teamAminus.setEnabled(true);
+                }
+                teamAscore.setText(String.valueOf(totalpointsA));
+                break;
+            case R.id.fbplusteamb:
+                rbCheck();
+                totalpointsB += numPoints;
+                if(totalpointsB > 0){
+                    teamBminus.setEnabled(true);
+                }
+                teamBscore.setText(String.valueOf(totalpointsB));
+                break;
+            case R.id.fbminusteama:
+                rbCheck();
+                totalpointsA -= numPoints;
+                if(totalpointsA<=0){
+                    totalpointsA = 0;
+                    teamAminus.setEnabled(false);
+                }
+                teamAscore.setText(String.valueOf(totalpointsA));
+                break;
+            case R.id.fbminusteamb:
+                rbCheck();
+                totalpointsB -= numPoints;
+                if(totalpointsB<=0){
+                    totalpointsB = 0;
+                    teamBminus.setEnabled(false);
+                }
+                teamBscore.setText(String.valueOf(totalpointsB));
+                break;
+
+        }
+
+    }
+
+    public void rbCheck(){
+        RadioGroup pointsgroup = findViewById(R.id.pointsgroup);
+
+        int checkedPoints = pointsgroup.getCheckedRadioButtonId();
+
+        if(checkedPoints == R.id.rb1point){
+            numPoints = 1;
+        }
+        else if(checkedPoints == R.id.rb2points){
+            numPoints = 2;
+        }
+        else if(checkedPoints == R.id.rb3points){
+            numPoints = 3;
+        }
+        else if(checkedPoints == R.id.rb6points){
+            numPoints = 6;
+        }
+    }
+
+
 }
